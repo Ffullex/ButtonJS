42c2b47e46206d03b755216671cf435bc56b04c7
ffullex <tomanovskiyyv@mer.ci.nsu.ru>
2021-04-12 23:42:12 +0700

    Some refactor for smoother movement Немного рефакторинга для более плавного движения

--- main.js
+++ main.js
@@ -1,34 +1,37 @@
 let block = document.getElementById('block');
 
 // обработка события "при нажатой ЛКМ"
-block.onmousedown = function(event) {
+block.onmousedown = function (event) {
 
-    // Сдвиги по X и Y, отнимаем от клиентских координат размер и позицию block
-    // Нужны для того, чтобы избежать центрирования курсора
+    // убираем рамочку выделения
+    event.preventDefault();
+
+    // сдвиги по X и Y, отнимаем от клиентских координат размер и позицию block
+    // нужны для того, чтобы избежать центрирования курсора
     let shiftX = event.clientX - block.getBoundingClientRect().left;
     let shiftY = event.clientY - block.getBoundingClientRect().top;
 
-    // Двигает block по экрану, меняя его координаты
+    // двигает block по экрану, меняя его координаты
     function moveAt(pageX, pageY) {
         block.style.left = pageX - shiftX + 'px';
         block.style.top = pageY - shiftY + 'px';
     }
 
-
     // вызывает функцию moveAt при обработке события
     // осуществляет плавное передвижение блока
     function onMouseMove(event) {
-        return setTimeout(moveAt, 100, event.pageX, event.pageY);
+        setTimeout(moveAt, 150, event.pageX, event.pageY);
     }
 
     // регистрирует событие "движение мыши", вызывает функцию, которая тащит block
     document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
 
     // отпускает block, удаляет ненужные обработчики
-    block.onmouseup = function() {
+    function onMouseUp() {
+        document.removeEventListener('mouseup', onMouseUp);
         document.removeEventListener('mousemove', onMouseMove);
-        block.onmouseup = null;
-    };
+    }
 };
 
 // отключает Drag`n`Drop браузера, чтобы не было конфликта
