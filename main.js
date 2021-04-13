93bc129e640c50da6cc0d94a6b22ac31ace57841
ffullex <tomanovskiyyv@mer.ci.nsu.ru>
2021-04-13 11:21:30 +0700

    some redactor

--- main.js
+++ main.js
@@ -1,16 +1,7 @@
 let block = document.getElementById('block');
 
-// обработка события "при нажатой ЛКМ"
+
 block.onmousedown = function (event) {
-
-    // убираем рамочку выделения
-    event.preventDefault();
-
-    // сдвиги по X и Y, отнимаем от клиентских координат размер и позицию block
-    // нужны для того, чтобы избежать центрирования курсора
-    let shiftX = event.clientX - block.getBoundingClientRect().left;
-    let shiftY = event.clientY - block.getBoundingClientRect().top;
-
     // двигает block по экрану, меняя его координаты
     function moveAt(pageX, pageY) {
         block.style.left = pageX - shiftX + 'px';
@@ -23,16 +14,27 @@ block.onmousedown = function (event) {
         setTimeout(moveAt, 150, event.pageX, event.pageY);
     }
 
-    // регистрирует событие "движение мыши", вызывает функцию, которая тащит block
-    // при отпускании сразу вызывает функцию onMouseUp
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
-
     // отпускает block, удаляет ненужные обработчики
     function onMouseUp() {
         document.removeEventListener('mouseup', onMouseUp);
         document.removeEventListener('mousemove', onMouseMove);
     }
+
+    // убираем рамочку выделения
+    event.preventDefault();
+
+    // сдвиги по X и Y, отнимаем от клиентских координат размер и позицию block
+    // нужны для того, чтобы избежать центрирования курсора
+    const {left, top} = block.getBoundingClientRect();
+    let shiftX = event.clientX - left
+    let shiftY = event.clientY - top;
+
+    // регистрирует событие "движение мыши", вызывает функцию, которая тащит block
+    // при отпускании сразу вызывает функцию onMouseUp
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+
+
 };
 
 // отключает Drag`n`Drop браузера, чтобы не было конфликта
